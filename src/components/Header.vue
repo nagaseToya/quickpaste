a2428028208fdc1bdea246e72849ba1873b88254
長瀬柊哉 <touya.nagase@ge-ne-sis.com>
2020-06-22 15:16:37 +0900

    URL短縮のデザイン

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,10 +12,22 @@
     <div class="addCard" @click="setRoom(0)">新規保存</div>
     <div class="shorten" @click="shorten()">URL短縮水産</div>
     <div class="shorten-cantainer">
-      <input v-model="longUrl" type="text" />
-      <button @click="shorten()">短縮</button>
-      <div>{{ shortenUrl }}</div>
-      <button @click="copy()">コピー</button>
+      <div>
+        <input
+          class="input"
+          v-model="longUrl"
+          type="text"
+          placeholder="短縮したいURLを入力"
+        />
+        <div v-if="shortenUrl === ''">
+          <button class="p-button" @click="shorten()">短縮</button>
+        </div>
+      </div>
+
+      <div v-if="shortenUrl !== ''">
+        <div>{{ shortenUrl }}</div>
+        <button class="c-button" @click="copy()">コピー</button>
+      </div>
     </div>
     <div class="user-info-container" @click="signOut()">ログアウト</div>
   </header>
@@ -28,7 +40,11 @@ import router from "../router";
 
 export default {
   data: function() {
-    return { longUrl: "https://mae.chab.in/archives/2495", shortenUrl: "url" };
+    return {
+      num: 0,
+      longUrl: "https://mae.chab.in/archives/2495",
+      shortenUrl: "",
+    };
   },
   methods: {
     setRoom(id) {
@@ -69,6 +85,8 @@ export default {
       document.addEventListener("copy", listener);
       // コピー
       document.execCommand("copy");
+      this.shortenUrl = "";
+      this.longUrl = "";
     },
     signOut() {
       firebase
@@ -99,5 +117,40 @@ export default {
   border-radius: 0px 0px 20px 20px;
   line-height: 30px;
   font-family: "Noto Sans JP", sans-serif;
+}
+.input {
+  border: 1px solid; /* 枠線 */
+  border-radius: 0.67em; /* 角丸 */
+  padding: 0.5em; /* 内側の余白量 */
+  margin: 10% 0% 4% 0%;
+  background-color: rgb(225, 225, 225); /* 背景色 */
+  width: 70%; /* 横幅いっぱいにする */
+  box-sizing: border-box; /* ※これがないと横にはみ出る */
+  height: 20%; /* 高さ */
+  line-height: 1.2; /* 行の高さ */
+  z-index: 10;
+}
+.input:focus {
+  border-color: skyblue; /* 枠線の色 */
+  background-color: #c7ddd8; /* 背景色 */
+  outline: none;
+}
+.p-button {
+  border-radius: 0.67em;
+  border: 1px solid #454545; /* 枠線 */
+  margin: 8% 0% 4% 0%;
+}
+.p-button:hover {
+  background-color: #e1e1e1; /* 背景色 */
+  border: 1.5px solid #e64e40; /* 枠線 */
+}
+.c-button {
+  border-radius: 0.67em;
+  border: 1px solid #454545; /* 枠線 */
+  margin: 4% 0% 4% 0%;
+}
+.c-button:hover {
+  background-color: #e1e1e1; /* 背景色 */
+  border: 1.5px solid #e64e40; /* 枠線 */
 }
 </style>
